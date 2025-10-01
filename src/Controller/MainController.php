4ab17042efa35d61fec050f689ff14650ead4ca5
arowana755 <silure.38@gmail.com>
2025-10-01 20:01:46 +0200

    Deuxième commit, ajoutant le contenu. Il sera refait.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,36 @@ final class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
+    #[Route('/competences', name: 'app_competences')]
+    public function competences(): Response
+    {
+        return $this->render('main/competences.html.twig', [
+            'controller_name' => 'CompetencesController',
+        ]);
+    }
+
+    #[Route('/infos_perso', name: 'app_infos_perso')]
+    public function infos_perso(): Response
+    {
+        return $this->render('main/infos_perso.html.twig', [
+            'controller_name' => 'InfosPersoController',
+        ]);
+    }
+
+    #[Route('/projets', name: 'app_projets')]
+    public function projets(): Response
+    {
+        return $this->render('main/projets.html.twig', [
+            'controller_name' => 'ProjetsController',
+        ]);
+    }
 }
