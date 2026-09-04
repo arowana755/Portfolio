66bc89158806f77bb86dfefbc7f79ea8174beedf
arowana755 <silure.38@gmail.com>
2026-09-05 00:25:24 +0200

    Put everything on one page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,58 +19,6 @@ final class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-        ]);
-    }
-
-    #[Route(
-        path: '/{_locale}/contact',
-        name: 'port_contact',
-        requirements: ['locale' => '%app.supported_locales%'],
-        defaults: ['_locale' => 'fr']
-    )]
-    public function contact(): Response
-    {
-        return $this->render('main/contact.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
-    }
-
-    #[Route(
-        path: '/{_locale}/competences',
-        name: 'port_competences',
-        requirements: ['locale' => '%app.supported_locales%'],
-        defaults: ['_locale' => 'fr']
-    )]
-    public function competences(): Response
-    {
-        return $this->render('main/competences.html.twig', [
-            'controller_name' => 'CompetencesController',
-        ]);
-    }
-
-    #[Route(
-        path: '/{_locale}/infos_perso',
-        name: 'port_infos_perso',
-        requirements: ['locale' => '%app.supported_locales%'],
-        defaults: ['_locale' => 'fr']
-    )]
-    public function infos_perso(): Response
-    {
-        return $this->render('main/infos_perso.html.twig', [
-            'controller_name' => 'InfosPersoController',
-        ]);
-    }
-
-    #[Route(
-        path: '/{_locale}/projets',
-        name: 'port_projets',
-        requirements: ['locale' => '%app.supported_locales%'],
-        defaults: ['_locale' => 'fr']
-    )]
-    public function projets(): Response
-    {
-        return $this->render('main/projets.html.twig', [
-            'controller_name' => 'ProjetsController',
         ]);
     }
 }
